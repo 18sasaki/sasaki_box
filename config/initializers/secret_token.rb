c82687e306e26dd8077dc13e6eb3fcdc6bfaa6ed
18sasaki <18sasaki@gmail.com>
2014-10-14 01:08:28 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SasakiBox::Application.config.secret_key_base = 'a8bdb5e1bbcf1c39c490f1d740b9d6f8d8dd3be65085d8036cd8a91dc99fb55dbec7fe0ebbfb851af6429ea44330fd00c1c90b459ea857ae0ed6d6bb81698ae1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SasakiBox::Application.config.session_store :cookie_store, key: '_sasaki_box_session'
